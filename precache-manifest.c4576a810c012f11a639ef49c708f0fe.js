39fdc2cdc7503a770e587a69f81d6cc50d81b847
Panagiotis_Karaliolios <karaliolios.panagiotis@gmail.com>
2021-11-01 22:31:08 +0200

    Updates

---
+++ precache-manifest.c4576a810c012f11a639ef49c708f0fe.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "79be87cd67e012bd70a90f0cf085548a",
+    "url": "/Portfolio/index.html"
+  },
+  {
+    "revision": "0110437fe6089ee6ccba",
+    "url": "/Portfolio/static/css/main.9e616e28.chunk.css"
+  },
+  {
+    "revision": "bb9a1428a7faaf1c4a76",
+    "url": "/Portfolio/static/js/2.3908a44e.chunk.js"
+  },
+  {
+    "revision": "8f06b9e0c45e6669bf287c6947cdbc57",
+    "url": "/Portfolio/static/js/2.3908a44e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0110437fe6089ee6ccba",
+    "url": "/Portfolio/static/js/main.d74f9d15.chunk.js"
+  },
+  {
+    "revision": "b83a37198ba343383876",
+    "url": "/Portfolio/static/js/runtime-main.e440ee5a.js"
+  },
+  {
+    "revision": "133f74d413430d5cbcb5ec96a3da5ef0",
+    "url": "/Portfolio/static/media/Discount.133f74d4.svg"
+  },
+  {
+    "revision": "5812367886ba07e186606d5e69883829",
+    "url": "/Portfolio/static/media/Karaliolios_Panagiotis-CV.58123678.pdf"
+  },
+  {
+    "revision": "9d21cdb974ca0a79386a74cedd77f2df",
+    "url": "/Portfolio/static/media/Profile_Picture (2).9d21cdb9.svg"
+  },
+  {
+    "revision": "43dc866eb350f3e507fdaccff4297527",
+    "url": "/Portfolio/static/media/moon.43dc866e.svg"
+  },
+  {
+    "revision": "4c60595c68f87842cf2ce5b1220b1b34",
+    "url": "/Portfolio/static/media/portrait.4c60595c.svg"
+  },
+  {
+    "revision": "dfba6a340f1c50121de9965d712c938c",
+    "url": "/Portfolio/static/media/sunny-outline.dfba6a34.svg"
+  }
+]);
